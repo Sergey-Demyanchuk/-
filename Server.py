4ccd8c5464a0f0a96604cd4ae00e2775fce0012c
Sergey-Demyanchuk <kotofeysuper2.0@gmail.com>
2024-03-04 17:23:08 +1000

    1-ый запуск

--- Server.py
+++ Server.py
@@ -8,11 +8,20 @@ main_socket.bind(("localhost",10000))#Привязка айпи к порту
 main_socket.setblocking(False)#Настраиваем непрерывность
 main_socket.listen(5)#Количество участников
 print("Сокет создался")
-
+players = []
 while True:
     try:
     #Проверяем желающий присоединиться
         new_socket, addr = main_socket.accept()#Принимаем входящие данные
         print("Подключился!", addr)
+        new_socket.setblocking(False)
+        players.append(new_socket)
     except BlockingIOError:
+        pass
+#Подсчёт комманды игроков
+for sock in players:
+    try:
+        data = sock.recv(1024).decode() #Обработка сообщений по 1024 байта
+        print("Получил", data)
+    except:
         pass
